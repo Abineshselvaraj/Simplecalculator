42f6532685471d4bf8f890fe82bbf7e3b55e83dd
Abinesh Selvaraj <abinesh@gmail.com>
2026-04-17 15:40:34 +0530

    btm

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <?php
               }else{
                 ?>
-                Result will appear here! 
+                The final Result is! 
                <?php
               }
               ?>
